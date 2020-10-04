bb730e827ead56c68d318703e0eb75ba2509c0a0
Mitali92 <60850783+Mitali92@users.noreply.github.com>
2020-10-04 15:57:03 -0400

    Add files via upload

---
+++ tempSimu.bat
@@ -0,0 +1,2 @@
+cd /D "C:\eclipse\workspace\A2CellDEVS"
+"C:/eclipse/plugins/cdBuilder.simulator_1.0.0.201109022248/internal/simuOrig.exe"  -m"Region.ma" -o"RegionOUT.out" -l"RegionLOG.log" -t"00:02:00:000" 
